9e03cfcab852edfc74effffa6552122916b85272
Rezhe <rezhewang@gmail.com>
2025-11-23 20:10:50 +1100

    unimproved algo

---
+++ target/release/prime-cli.d
@@ -0,0 +1,1 @@
+D:\allens\code\traetest\6\target\release\prime-cli.exe: D:\allens\code\traetest\6\src\lib.rs D:\allens\code\traetest\6\src\main.rs D:\allens\code\traetest\6\src\prime_sieve.rs
---
+++ target/release/prime-gui.d
@@ -0,0 +1,1 @@
+D:\allens\code\traetest\6\target\release\prime-gui.exe: D:\allens\code\traetest\6\src\gui_main.rs D:\allens\code\traetest\6\src\lib.rs D:\allens\code\traetest\6\src\prime_sieve.rs
